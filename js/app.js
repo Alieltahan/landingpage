c3072ca5a8e1e50a323bebe5a393e858f64970a6
Alieltahan <alyaltahan@gmail.com>
2021-06-16 13:52:05 +0200

    Completed Smooth Function

--- js/app.js
+++ js/app.js
@@ -35,30 +35,45 @@ const numberOfSections = sections.length;
 // Virtual DOM
 // const frag = document.createDocumentFragment();
 
-/**
- * @description:  Dynamic Nav List Function based on Number of Sections.
- * @param : Number of Sections.
- */
+// @description:  Dynamic Nav List Function based on Number of Sections.
+// @param : Number of Sections.
+
 const dynamicNavList = function (num) {
-  /**
-   * Looping over the sections Array's to get:
-   * 1- Section Name to be displayed as list textContent.
-   * 2- ID which to be used as href for the list.
-   */
+  //  Looping over the sections Array's to get:
+  //  1- Section Name to be displayed as list textContent.
+  //  2- Adding data-* attribute to be used later for smooth Scrolling.
+
   for (let i = 0; i < num; i++) {
-    const sectionsName = sections[i].getAttribute("data-nav");
-    const navHrefAtt = sections[i].getAttribute("id");
+    const sectionName = sections[i].getAttribute("data-nav");
+    const sectionId = sections[i].getAttribute("id");
 
     const list = document.createElement("li");
     const link = document.createElement("a");
-    link.setAttribute("href", "#" + navHrefAtt);
-    link.innerText = sectionsName;
+    link.setAttribute("data-sectionId", "#" + sectionId);
+    link.innerText = sectionName;
     link.classList.add("menu__link");
-    // list.classList.add("menu__link");
     navBar.append(list);
     list.append(link);
+
+    // smoothScroll(sectionId);
   }
 };
+
+// Adding Smooth Scrolling Function based on data-* attribute.
+
+navBar.addEventListener("click", function (e) {
+  const section = document.querySelector(
+    e.target.getAttribute("data-sectionId")
+  );
+  section.scrollIntoView({ behavior: "smooth" });
+});
+
+// const smoothScroll = function (el) {
+//   el.scrollIntoView({ behavior: "smooth" });
+// };
+
+// const section1 = document.querySelector("#section1");
+// section1.scrollIntoView({ behavior: "smooth" });
 
 /**
  * End Helper Functions
